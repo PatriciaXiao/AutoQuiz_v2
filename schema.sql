d2409fd51a8ba0e09d2cf6b88bf1dd203d45cfbe
Patricia Xiao <patricia.xiao@berkeley.edu>
2018-03-01 09:24:21 -0800

    designing the records table

--- schema.sql
+++ schema.sql
@@ -1,9 +1,21 @@
 drop table if exists users;
+drop table if exists skills;
+drop table if exists questions;
 drop table if exists records;
 create table users (
   id integer primary key autoincrement,
   name string not null,
   password string not null
+);
+create table skills (
+  id integer primary key autoincrement,
+  name string not null,
+  description string
+);
+create table questions (
+  id integer primary key autoincrement,
+  description string not null,
+  skill integer not null
 );
 create table records (
   id integer primary key autoincrement,
