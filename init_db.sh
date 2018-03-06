dda9cc13c3d1f1000cae441c10d17c81396bf4a9
Patricia Xiao <patricia.xiao@berkeley.edu>
2018-03-05 20:02:23 -0800

    shell script updated

--- init_db.sh
+++ init_db.sh
@@ -1,2 +1,14 @@
-sqlite3 auto_quiz.db < schema.sql
+if [ "$1" == "" ]; then
+    echo "Updating database (not initializing) by default"
+else
+    if [ "$1" != "-init" ]; then
+        echo "Couldn't recognize your command $1, please try again latter"
+    else
+        echo "Initializing the database...."
+        sqlite3 auto_quiz.db < schema.sql
+        echo "Finished initializing"
+fi
+    sqlite3 auto_quiz.db < schema.sql
+fi
+
 python init_db.py
