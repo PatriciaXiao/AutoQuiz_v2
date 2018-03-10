c54550fd6abc615cb34f06417f490dddceff10e5
PatriciaXiao <patricia.xiao@berkeley.edu>
2018-03-10 08:24:47 +0000

    tiny bug fixed

--- init_db.sh
+++ init_db.sh
@@ -7,7 +7,7 @@ else
         echo "Couldn't recognize your command $1, please try again latter"
     else
         echo "Initializing the database...."
-        sqlite auto_quiz.db < schema.sql
+        sqlite3 auto_quiz.db < schema.sql
         echo "Finished initializing"
         python init_db.py
         echo "Finished loading content"
